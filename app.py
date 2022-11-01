38f89a69a0a089d3419a2dbf09b048d039af7906
Laurel McLean <laurel.mclean@live.ca>
2022-11-01 11:35:41 -0600

    Completed favorite things

--- app.py
+++ app.py
@@ -41,12 +41,24 @@ def show_froyo_results():
 @app.route('/favorites')
 def favorites():
     """Shows the user a form to choose their favorite color, animal, and city."""
-    pass
+    return """
+    <form action="/favorite_results" method="GET">
+        What is your favorite color? <br/>
+        <input type="text" name="color"><br/>
+        What is your favorite animal? <br/>
+        <input type="text" name="animal"><br/>
+        What is your favorite city? <br/>
+        <input type="text" name="city"><br/>
+        <input type="submit" value="Submit!">
+    </form>
+    """
 
 @app.route('/favorites_results')
 def favorites_results():
-    """Shows the user a nice message using their form results."""
-    pass
+    users_color = request.args.get('color')
+    users_animal = request.args.get('animal')
+    users_city = request.args.get('city')
+    return f"Wow, I didn't know {users_color} {users_animal} lived in {users_city}!"
 
 @app.route('/secret_message')
 def secret_message():
