55a9f66191fec55cecfe8451ebc4ad30d6a42b34
Laurel McLean <laurel.mclean@live.ca>
2022-11-01 12:20:37 -0600

    Refactored fro yo with render templates

--- app.py
+++ app.py
@@ -21,22 +21,16 @@ def homepage():
 
 @app.route('/froyo')
 def choose_froyo():
-    """Shows a form to collect the user's Fro-Yo order."""
-    return """
-    <form action="/froyo_results" method="GET">
-        What is your favorite Fro-Yo flavor? <br/>
-        <input type="text" name="flavor"><br/>
-        What toppings would you like? <br/>
-        <input type="text" name="toppings"><br/>
-        <input type="submit" value="Submit!">
-    </form>
-    """
+    return render_template('froyo_form.html')
+
 # request.args is a dictionary storing data as key value pairs we can retrieve with get
 @app.route('/froyo_results')
 def show_froyo_results():
-    users_froyo_flavor = request.args.get('flavor')
-    users_toppings = request.args.get('toppings')
-    return f'You ordered {users_froyo_flavor} flavored Fro-Yo with toppings {users_toppings}!'
+    context = {
+        'users_froyo_flavor' : request.args.get('flavor'),
+        'users_toppings' : request.args.get('toppings')
+    }
+    return render_template('froyo_results.html', **context)
 
 @app.route('/favorites')
 def favorites():
